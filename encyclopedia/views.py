8f886188eb8bfad7c3f093aa90fc9749d3498308
Martin Coronel <martincxx@gmail.com>
2023-04-22 20:57:02 +0300

    Implement search function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 import markdown2
 from . import util
 
@@ -17,3 +19,17 @@ def entry_page(request, title):
     else:
         html_content = markdown2.markdown(entry)
     return render(request, "encyclopedia/entry.html", {"title": title, "content": html_content})
+
+
+def search(request):
+    title = request.GET.get("q", None)
+    entries = util.list_entries()
+    if title in entries:
+        return HttpResponseRedirect(reverse("entry_page", kwargs={"title": title}))
+
+    not_found = """
+    <h1>Search results</h1>
+    <p>//TODO</p>"""
+
+    return render(request, "encyclopedia/search_results.html",
+                  {"title": title, "content": not_found})
